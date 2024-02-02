7b4b60e8891bd8976c8c3a0e51bc20e23b8354a7
hourglass2020 <pouria.fm15@gmail.com>
2024-02-02 19:49:39 +0330

    Modify ping class and get packet size input

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ping import Ping
 
 hostname = input("Enter hostname: ")
 packet_number = int(input("Enter packet number: "))
+packet_size = int(input("Enter packet size: (like 64/32) "))
 
-ping = Ping(packet_num=packet_number, hostname=hostname)
+ping = Ping(packet_num=packet_number, hostname=hostname, packet_size=packet_size)
 ping.calculate_ping()
--- ping.py
+++ ping.py
@@ -6,10 +6,7 @@ import os
 
 import select
 
-default_timer = time.time
-
 ICMP_MODE = socket.getprotobyname("icmp")
-PACKET_SIZE = 64
 WAIT_TIMEOUT = 3000.0
 MAX_SLEEP = 1000
 
@@ -19,10 +16,10 @@ ICMP_MAX_RECV = 2048
 
 
 class Ping:
-    def __init__(self, packet_num, hostname):
+    def __init__(self, packet_num, hostname, packet_size):
         self.destination_ip = None
         self.server_socket = None
-
+        self.packet_size = packet_size
         self.packet_num = packet_num
         self.hostname = hostname
         self._id = os.getpid() & 0xFFFF
@@ -31,28 +28,24 @@ class Ping:
         try:
             self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_MODE)
         except socket.error as e:
-            print(f"failed. (socket error: {e.args[1]})")
+            print(f"An error ocurred: {e.args[1]})")
             raise
 
     def calculate_ping(self):
         try:
             self.destination_ip = socket.gethostbyname(self.hostname)
-            print(f"Ping {self.hostname}")
+            print(f"\nPing {self.hostname}")
             print(f"Destination IP: {self.destination_ip}")
-            print(f"Packet size: {PACKET_SIZE}\n\n")
+            print(f"Packet size: {self.packet_size}\n\n")
         except socket.gaierror as e:
             print(f"\nError: Unknown host: {self.hostname} ({e.args[1]})")
             return
 
-        sequence_count = 0
-
-        for _ in range(self.packet_num):
-            delay = self.get_ping(sequence_count)
+        for index in range(self.packet_num):
+            delay = self.get_ping(index)
 
             if delay is None:
                 delay = 0
-
-            sequence_count += 1
 
             if MAX_SLEEP > delay:
                 time.sleep((MAX_SLEEP - delay) / 1000)
@@ -80,7 +73,7 @@ class Ping:
 
         return delay
 
-    def send_packet(self, sequence_count):
+    def create_packet(self, sequence_count):
         checksum_num = 0
 
         header = struct.pack("!BBHHH", ICMP_ECHO, 0, checksum_num, self._id, sequence_count)
@@ -90,10 +83,10 @@ class Ping:
 
         if sys.version[:1] == '2':
             bytes_count = struct.calcsize("d")
-            data = ((PACKET_SIZE - 8) - bytes_count) * "Q"
-            data = struct.pack("d", default_timer()) + data
+            data = ((self.packet_size - 8) - bytes_count) * "Q"
+            data = struct.pack("d", time.time()) + data
         else:
-            for i in range(start_val, start_val + (PACKET_SIZE - 8)):
+            for i in range(start_val, start_val + (self.packet_size - 8)):
                 pad_bytes += [(i & 0xff)]
             data = bytearray(pad_bytes)
 
@@ -102,13 +95,15 @@ class Ping:
         header = struct.pack("!BBHHH", ICMP_ECHO, 0, checksum_num, self._id, sequence_count)
 
         packet = header + data
+        return packet
 
-        send_time = default_timer()
+    def send_packet(self, sequence_count):
+        send_time = time.time()
 
         try:
-            self.server_socket.sendto(packet, (self.destination_ip, 1))
+            self.server_socket.sendto(self.create_packet(sequence_count), (self.destination_ip, 1))
         except socket.error as e:
-            print(f"General failure ({e.args[1]})")
+            print(f"An error occurred: ({e.args[1]})")
             return
 
         return send_time
@@ -118,14 +113,14 @@ class Ping:
         time_left = WAIT_TIMEOUT / 1000
 
         while True:
-            started_select = default_timer()
+            started_select = time.time()
             what_ready = select.select([self.server_socket], [], [], time_left)
-            how_long_in_select = (default_timer() - started_select)
+            how_long_in_select = (time.time() - started_select)
 
-            if not what_ready[0]:  # Timeout
+            if not what_ready[0]:
                 return None, 0, 0, 0, 0
 
-            time_received = default_timer()
+            time_received = time.time()
 
             rec_packet, addr = self.server_socket.recvfrom(ICMP_MAX_RECV)
 
@@ -139,28 +134,28 @@ class Ping:
             if time_left <= 0:
                 return None, 0, 0, 0, 0
 
-    def checksum(self, source_string):
-        count_to = (int(len(source_string) / 2)) * 2
+    def checksum(self, data):
+        count_to = (int(len(data) / 2)) * 2
         summ = 0
         count = 0
 
-        low_byte = 0
-        high_byte = 0
+        low_b = 0
+        high_b = 0
         while count < count_to:
             if sys.byteorder == "little":
-                low_byte = source_string[count]
-                high_byte = source_string[count + 1]
+                low_b = data[count]
+                high_b = data[count + 1]
             else:
-                low_byte = source_string[count + 1]
-                high_byte = source_string[count]
+                low_b = data[count + 1]
+                high_b = data[count]
 
-            summ = summ + (high_byte * 256 + low_byte)
+            summ = summ + (high_b * 256 + low_b)
 
             count += 2
 
-        if count_to < len(source_string):
-            low_byte = source_string[len(source_string) - 1]
-            summ += low_byte
+        if count_to < len(data):
+            low_b = data[len(data) - 1]
+            summ += low_b
 
         summ &= 0xffffffff
 
